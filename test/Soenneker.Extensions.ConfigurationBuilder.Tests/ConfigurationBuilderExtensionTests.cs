6ac15cb4672fa7b1df92735a422f3b2843650c99
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:50 -0500

    TUnit conversion

--- test/Soenneker.Extensions.ConfigurationBuilder.Tests/ConfigurationBuilderExtensionTests.cs
+++ test/Soenneker.Extensions.ConfigurationBuilder.Tests/ConfigurationBuilderExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.ConfigurationBuilder.Tests;
 
 public sealed class ConfigurationBuilderExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
