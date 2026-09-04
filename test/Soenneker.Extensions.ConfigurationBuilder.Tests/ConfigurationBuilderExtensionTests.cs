50a5fd471857d12a1f2f61b3400f649a96e52d0b
soenneker <jake@soenneker.com>
2026-09-04 12:09:09 -0500

    Deps

--- test/Soenneker.Extensions.ConfigurationBuilder.Tests/ConfigurationBuilderExtensionTests.cs
+++ test/Soenneker.Extensions.ConfigurationBuilder.Tests/ConfigurationBuilderExtensionTests.cs
@@ -1,6 +1,4 @@
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
 using Soenneker.Tests.Unit;
 
@@ -9,7 +7,7 @@ namespace Soenneker.Extensions.ConfigurationBuilder.Tests;
 public sealed class ConfigurationBuilderExtensionTests : UnitTest
 {
     [Test]
-    public async Task AddAppSettings_canonicalizes_known_environment_casing()
+    public async System.Threading.Tasks.Task AddAppSettings_canonicalizes_known_environment_casing()
     {
         var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
 
@@ -20,7 +18,7 @@ public sealed class ConfigurationBuilderExtensionTests : UnitTest
     }
 
     [Test]
-    public async Task AddOcelotConfig_uses_base_file_for_unknown_environment()
+    public async System.Threading.Tasks.Task AddOcelotConfig_uses_base_file_for_unknown_environment()
     {
         var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
 
